e818a988dfc8c1afd29d6efd99a5792bbbda5a04
Eduardo Rossi <edurossi@gmail.com>
2019-08-02 11:57:33 -0500

    Phaser configured

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "./src/main.js",
   output: {
     sourcemap: true,
     inputSourceMap: true,
-    file: "./public/main.min.js",
+    file: "./public/js/main.js",
     format: "iife",
     name: "bundle",
+    globals: {
+      phaser: "Phaser",
+    },
+  },
+  external: ["phaser"],
+  watch: {
+    include: "src/**",
   },
   plugins: [
     babel({
@@ -17,5 +25,8 @@ export default {
     }),
     resolve(),
     commonjs(),
+    copy({
+      targets: [{ src: "node_modules/phaser/dist/phaser.js", dest: "public/js/" }],
+    }),
   ],
 };
--- src/test.js
+++
@@ -1,5 +0,0 @@
-const sayHello = (message) => {
-    console.log(message);
-}
-
-export default sayHello;
